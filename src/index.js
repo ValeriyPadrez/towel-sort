95a78d5f0e16ecd0036f3ffbff8231a8600b566f
Valeriy <valerypadrez@yandex.by>
2025-09-07 22:41:22 +0300

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) return [];
+
+  let result = [];
+
+  matrix.forEach((row, index) => {
+    if (index % 2 === 1) {
+      result = result.concat(row.slice().reverse());
+    } else {
+      result = result.concat(row);
+    }
+  });
+
+  return result;
 };
